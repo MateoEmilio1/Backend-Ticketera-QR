afbbc96d7ab5954e7507ee99d9209e7cdaf87afc
NicolasDiDio <NicolasDiDio09@gmail.com>
2025-02-28 12:43:37 -0300

    usuarioControllerAdded

---
+++ src/controllers/usuario.controller.ts
@@ -0,0 +1,29 @@
+import{prisma} from '../prisma.js';
+import { Request, Response } from 'express';
+
+const crearUsuario = async (req: Request, res: Response):Promise<void> => {
+  try {
+    const { mail, contraseña } = req.body;
+    const usuario = await prisma.usuario.create({
+      data: {
+        mail,
+        contraseña,
+        rol: "ADMIN",
+      },
+    });
+    res.status(201).json({
+      message: "Usuario creado con éxito",
+      data: usuario,
+      error: false,
+    });
+  } catch (error) {
+    console.error("Error en crearUsuario:", error);
+    res.status(500).json({
+      message: "Error al crear el usuario",
+      error: true,
+      details: (error as Error).message,
+    });
+  }
+}
+
+export default { crearUsuario };
